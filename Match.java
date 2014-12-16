0b4f8f22e93a8f69629c001755b83177a58d1dae
MickeyParkman <wrestlingparkman@gmail.com>
2014-12-15 23:11:46 -0800

    Match Class

--- Match.java
+++ Match.java
@@ -3,12 +3,26 @@ public class Match{
    private int matchNumber;
    
    private String wrestler1, wrestler2;
+   private int winner;
    
    //stores the information on how the wrestler won. ie pin 1:23
    String winStatement;
    
    public Match(int matchNumber){
-      
+      this.matchNumber = matchNumber;
+   }
+   
+   public void setWinner(int winner){
+      this.winner = winner;
+   }
+   
+   public String getWinner(){
+      if(winner == 1)
+         return wrestler1;
+      else if(winner == 2)
+         return wrestler2;
+      else   
+         return "";
    }
    
 }
