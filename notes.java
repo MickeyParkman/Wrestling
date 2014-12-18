882e639ba5545be68d403d8ae9c19bf6ce26fed1
MickeyParkman <wrestlingparkman@gmail.com>
2014-12-18 12:57:47 -0800

    Notes

--- notes.java
+++ notes.java
@@ -1,2 +1,14 @@
 // Things we need
 // Something to track wins by By. If someone has a bye,
+   public static final int[] startIndexProportions = new int{0,7,14,17.5,21,22.75, 24.5};
+   int roundNum;
+   public Match(int matchNumber){
+      this.winner = -1;
+      this.matchNumber = matchNumber;
+      for(int i = 1; i < startIndexProportions.length; i++){
+         if(matchNumber < startIndexProportions[i]){
+            roundNum = i - 1;
+            break;  
+         }
+      }
+   }
