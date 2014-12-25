1d14f2798b4ee1a330ababb40ff24430eb25a15f
davidisSwag <davidzhai2003@yahoo.com>
2014-12-25 00:36:07 -0800

    Revert "Match Window"
    This reverts commit 59a74f74e075468d2595cdd9c3fbec63047ed9e6.

--- Client/MatchWindow.java
+++ Client/MatchWindow.java
@@ -16,19 +16,16 @@ public class MatchWindow extends JPanel implements Runnable{
    private JLabel wrestlerScore1;
    private JLabel wrestlerScore2;
    private JLabel time;
-   private JLabel period;
    private int score1;
    private int score2;
    private int periodNum = 1;
+   final JPanel thisPanel;
    
    private static boolean running = true;
-   private static boolean hasWinner = false;
    private static boolean counting = false;
    
-   private final MatchWindow thisMatch;
-   
-   public MatchWindow(String wrestler1Name, String wrestler2Name){
-      thisMatch = this;
+   public MatchWindow(){
+      thisPanel = this;
       minutes = lastMinutes = 2;
       seconds = lastSeconds = 0;
       score1 = 0;
@@ -37,7 +34,7 @@ public class MatchWindow extends JPanel implements Runnable{
             
       GridBagLayout gbl = new GridBagLayout();
       gbl.columnWidths = new int[]{WINDOW_WIDTH / 16, WINDOW_WIDTH / 8, WINDOW_WIDTH / 8, WINDOW_WIDTH / 8, WINDOW_WIDTH / 8, WINDOW_WIDTH / 8, WINDOW_WIDTH / 8, WINDOW_WIDTH / 8, WINDOW_WIDTH / 16};
-      gbl.rowHeights = new int[]{WINDOW_HEIGHT / 12, WINDOW_HEIGHT / 32, WINDOW_HEIGHT / 16, WINDOW_HEIGHT / 32, WINDOW_HEIGHT / 25, WINDOW_HEIGHT / 9, WINDOW_HEIGHT / 25, WINDOW_HEIGHT / 9, WINDOW_HEIGHT / 25, WINDOW_HEIGHT / 9, WINDOW_HEIGHT / 10, WINDOW_HEIGHT / 10, WINDOW_HEIGHT / 10, WINDOW_HEIGHT / 8};
+      gbl.rowHeights = new int[]{WINDOW_HEIGHT / 8, WINDOW_HEIGHT / 32, WINDOW_HEIGHT / 16, WINDOW_HEIGHT / 32, WINDOW_HEIGHT / 25, WINDOW_HEIGHT / 9, WINDOW_HEIGHT / 25, WINDOW_HEIGHT / 9, WINDOW_HEIGHT / 25, WINDOW_HEIGHT / 9, WINDOW_HEIGHT / 10, WINDOW_HEIGHT / 10, WINDOW_HEIGHT / 10};
       setLayout(gbl);
       
       GridBagConstraints gbc = new GridBagConstraints();
@@ -50,27 +47,18 @@ public class MatchWindow extends JPanel implements Runnable{
       wrestlerScore1.setForeground(color1);
       wrestlerScore1.setFont(new Font("Times New Roman", Font.PLAIN, 60));
       wrestlerScore1.setHorizontalAlignment(JLabel.CENTER);
-      wrestlerScore1.setVerticalAlignment(JLabel.CENTER);
-      add(wrestlerScore1, gbc);      
+      add(wrestlerScore1, gbc);
       
       gbc.gridx = 5;
+      gbc.gridy = 0;
+      gbc.gridwidth = 3;
+      gbc.fill = GridBagConstraints.BOTH;
+
       wrestlerScore2 = new JLabel(String.valueOf(score2));
       wrestlerScore2.setForeground(color2);
       wrestlerScore2.setFont(new Font("Times New Roman", Font.PLAIN, 60));
       wrestlerScore2.setHorizontalAlignment(JLabel.CENTER);
-      wrestlerScore2.setVerticalAlignment(JLabel.CENTER);
       add(wrestlerScore2, gbc);
-      
-      gbc.gridy = 1;
-      gbc.gridx = 1;
-      JLabel wrestler1 = new JLabel(wrestler1Name);
-      wrestler1.setHorizontalAlignment(JLabel.CENTER);
-      add(wrestler1, gbc);
-      
-      gbc.gridx = 5;
-      JLabel wrestler2 = new JLabel(wrestler2Name);
-      wrestler2.setHorizontalAlignment(JLabel.CENTER);
-      add(wrestler2, gbc);
       
       if(seconds < 10){
          time = new JLabel(minutes + ":0" + seconds);
@@ -78,7 +66,6 @@ public class MatchWindow extends JPanel implements Runnable{
          time = new JLabel(minutes + ":" + seconds);
       }
       time.setHorizontalAlignment(JLabel.CENTER);
-      time.setVerticalAlignment(JLabel.CENTER);
       time.setFont(new Font("Times New Roman", Font.PLAIN, 60));
       gbc.gridx = 3;
       gbc.gridy = 0;
@@ -93,26 +80,20 @@ public class MatchWindow extends JPanel implements Runnable{
       //play.setIcon();
       play.setFocusPainted(false);
       play.addActionListener(new ActionListener(){
-         public void actionPerformed(ActionEvent e){            
-            if(!running && (minutes != 0 || seconds != 0)){
-               new Thread(thisMatch).start();
-               counting = true;
-            }else if(!running){
-               counting = false;
-            }else{
-               counting = !counting;
-            }
+         public void actionPerformed(ActionEvent e){
+            
+            counting = !counting;
          }
       });
       
-      JButton reset = new JButton("Set");
+      JButton reset = new JButton("Reset");
       reset.setFocusPainted(false);
       reset.addActionListener(new ActionListener(){
          public void actionPerformed(ActionEvent e){
             if(!counting){
                final JFrame resetOptions = new JFrame("Set");
                resetOptions.setLayout(new FlowLayout());
-               resetOptions.setSize(new Dimension(250, 210));
+               resetOptions.setSize(new Dimension(200, 150));
                GridBagLayout gbl = new GridBagLayout();
                gbl.columnWidths = new int[]{100, 100};
                gbl.rowHeights = new int[]{50, 50, 50};
@@ -171,7 +152,7 @@ public class MatchWindow extends JPanel implements Runnable{
       timeControl.add(reset);
       add(timeControl, gbc);
       
-      period = new JLabel(" Period " + periodNum);
+      final JLabel period = new JLabel(" Period " + periodNum);
       period.setFont(new Font("Times New Roman", Font.PLAIN, 20));
       period.setHorizontalAlignment(JLabel.CENTER);
       gbc.gridy = 2;
@@ -185,8 +166,20 @@ public class MatchWindow extends JPanel implements Runnable{
       periodMinus.setFocusPainted(false);
       periodMinus.addActionListener(new ActionListener(){
          public void actionPerformed(ActionEvent e){
-            periodNum--;
-            setPeriod();
+            if(periodNum > 1){
+               periodNum--;
+               if(periodNum < 4){
+                  period.setText(" Period " + periodNum);
+               }else if(periodNum == 4){
+                  period.setText("Overtime");
+               }else if(periodNum == 5){
+                  period.setText("Tiebreaker 1");
+               }else if(periodNum == 6){
+                  period.setText("Tiebreaker 2");
+               }else if(periodNum == 7){
+                  period.setText("Ultimate Tiebreaker");
+               }
+            }
          }
       });
       periodChanger.add(periodMinus);
@@ -195,8 +188,20 @@ public class MatchWindow extends JPanel implements Runnable{
       periodPlus.setFocusPainted(false);
       periodPlus.addActionListener(new ActionListener(){
          public void actionPerformed(ActionEvent e){
-            periodNum++;
-            setPeriod();
+            if(periodNum < 7){
+               periodNum++;
+               if(periodNum < 4){
+                  period.setText(" Period " + periodNum);
+               }else if(periodNum == 4){
+                  period.setText("Overtime");
+               }else if(periodNum == 5){
+                  period.setText("Tiebreaker 1");
+               }else if(periodNum == 6){
+                  period.setText("Tiebreaker 2");
+               }else if(periodNum == 7){
+                  period.setText("Ultimate Tiebreaker");
+               }
+            }
          }
       });
       periodChanger.add(periodPlus);
@@ -217,65 +222,26 @@ public class MatchWindow extends JPanel implements Runnable{
       
       int buttonStartX = 1;
       int buttonStartY = 10;     
-      createButton("Takedown",         "T2",    buttonStartX,     buttonStartY,     1, color1);      
-      createButton("Escape",           "E1",    buttonStartX + 1, buttonStartY,     1, color1);      
-      createButton("Reversal",         "R2",    buttonStartX + 2, buttonStartY,     1, color1);      
-      createButton("2 Pt. Nearfall",   "N2",    buttonStartX,     buttonStartY + 1, 1, color1);      
-      createButton("3 Pt. Nearfall",   "N3",    buttonStartX + 1, buttonStartY + 1, 1, color1);
-      createButton("Tech. Violation",  "TV1",   buttonStartX + 2, buttonStartY + 1, 1, color1);
-      createButton("Stalling",   buttonStartX, buttonStartY + 2, 1, color1);
+      createButton("Takedown", "T2", buttonStartX, buttonStartY, 1, color1);      
+      createButton("Escape", "E1", buttonStartX + 1, buttonStartY, 1, color1);      
+      createButton("Reversal", "R2", buttonStartX + 2, buttonStartY, 1, color1);      
+      createButton("2 Pt. Nearfall", "N2", buttonStartX, buttonStartY + 1, 1, color1);      
+      createButton("3 Pt. Nearfall", "N3", buttonStartX + 1, buttonStartY + 1, 1, color1);
+      createButton("Tech. Violation", "TV1", buttonStartX + 2, buttonStartY + 1, 1, color1);
+      createButton("Stalling", buttonStartX, buttonStartY + 2, 1, color1);
       createButton("Blood Time", buttonStartX + 1, buttonStartY + 2, 1, color1);
-      createButton("Pin",        buttonStartX + 2, buttonStartY + 2, 1, color1);
+      createButton("Pin", buttonStartX + 2, buttonStartY + 2, 1, color1);
       
       int buttonStartX_2 = 5;
-      createButton("Takedown",         "T2",  buttonStartX_2,     buttonStartY,     1, color2);      
-      createButton("Escape",           "E1",  buttonStartX_2 + 1, buttonStartY,     1, color2);      
-      createButton("Reversal",         "R2",  buttonStartX_2 + 2, buttonStartY,     1, color2);      
-      createButton("2 Pt. Nearfall",   "N2",  buttonStartX_2,     buttonStartY + 1, 1, color2);      
-      createButton("3 Pt. Nearfall",   "N3",  buttonStartX_2 + 1, buttonStartY + 1, 1, color2);
-      createButton("Tech. Violation",  "TV1", buttonStartX_2 + 2, buttonStartY + 1, 1, color2);
-      createButton("Stalling",   buttonStartX_2, buttonStartY + 2, 1, color2);
+      createButton("Takedown", "T2", buttonStartX_2, buttonStartY, 1, color2);      
+      createButton("Escape", "E1", buttonStartX_2 + 1, buttonStartY, 1, color2);      
+      createButton("Reversal", "R2", buttonStartX_2 + 2, buttonStartY, 1, color2);      
+      createButton("2 Pt. Nearfall", "N2", buttonStartX_2, buttonStartY + 1, 1, color2);      
+      createButton("3 Pt. Nearfall", "N3", buttonStartX_2 + 1, buttonStartY + 1, 1, color2);
+      createButton("Tech. Violation", "TV1", buttonStartX_2 + 2, buttonStartY + 1, 1, color2);
+      createButton("Stalling", buttonStartX_2, buttonStartY + 2, 1, color2);
       createButton("Blood Time", buttonStartX_2 + 1, buttonStartY + 2, 1, color2);
-      createButton("Pin",        buttonStartX_2 + 2, buttonStartY + 2, 1, color2);
-      
-      JButton sendScore = new JButton("Send Score");
-      sendScore.addActionListener(new ActionListener(){
-         public void actionPerformed(ActionEvent e){
-            JFrame confirmation = new JFrame("End Match Confirmation");            
-            confirmation.setLayout(new BoxLayout(confirmation.getContentPane(), BoxLayout.Y_AXIS));
-                        
-            JLabel text = new JLabel("Type out the winner's name");
-            text.setAlignmentX(Component.CENTER_ALIGNMENT);
-            text.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-            confirmation.add(text);
-            
-            JTextField name = new JTextField();
-            name.setHorizontalAlignment(JTextField.CENTER);
-            name.setAlignmentX(Component.CENTER_ALIGNMENT);
-            name.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-            confirmation.add(name);
-            
-            JButton send = new JButton("Send");
-            send.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-            send.setAlignmentX(Component.CENTER_ALIGNMENT);
-            send.addActionListener(new ActionListener(){
-               public void actionPerformed(ActionEvent e){
-               //configure and then send the winning information to the server #swag
-               }
-            });
-            confirmation.add(send);
-            
-            confirmation.setSize(400, 200);
-            confirmation.setLocationRelativeTo(null);
-            confirmation.setVisible(true);
-         }
-      });
-      gbc.gridx = 1;
-      gbc.gridy = 13;
-      gbc.gridwidth = 7;
-      gbc.fill = GridBagConstraints.BOTH;
-      
-      add(sendScore, gbc);
+      createButton("Pin",buttonStartX_2 + 2, buttonStartY + 2, 1, color2); 
       
       new Thread(this).start();     
   }
@@ -300,30 +266,14 @@ public class MatchWindow extends JPanel implements Runnable{
       button.setForeground(color);
       button.addActionListener(new ActionListener(){
          public void actionPerformed(ActionEvent e){
-            final JButton scoreButton = new JButton(scoreDetails);
-            final int thisPeriod = periodNum;
+            JButton scoreButton = new JButton(scoreDetails);
             scoreButton.addActionListener(new ActionListener(){
                @Override
                public void actionPerformed(ActionEvent e){
-                  JPopupMenu menu = new JPopupMenu();
-                  JMenuItem delete = new JMenuItem("   Delete");
-                  delete.addActionListener(new ActionListener(){
-                     public void actionPerformed(ActionEvent e){
-                        paperDisplay.removeScoreButton(scoreButton, thisPeriod, gridx / 5 + 1);
-                        if(gridx / 5 == 0){
-                           score1 -= Character.getNumericValue(scoreDetails.charAt(scoreDetails.length() - 1));
-                           wrestlerScore1.setText(String.valueOf(score1));
-                        }else{
-                           score2 -= Character.getNumericValue(scoreDetails.charAt(scoreDetails.length() - 1));
-                           wrestlerScore2.setText(String.valueOf(score2));
-                        }
-                     }
-                  });
-                  menu.add(delete);
-                  menu.show(scoreButton,(int)(scoreButton.getMousePosition().getX() - 30), (int)(scoreButton.getMousePosition().getY() - 10));
+                  System.out.println(scoreDetails + " was pressed");
                }
             });
-            paperDisplay.addScoreButton(scoreButton, thisPeriod, gridx / 5 + 1);
+            paperDisplay.addScoreButton(scoreButton, periodNum, gridx / 5 + 1);
             if(gridx / 5 == 0){
                score1 += Character.getNumericValue(scoreDetails.charAt(scoreDetails.length() - 1));
                wrestlerScore1.setText(String.valueOf(score1));
@@ -359,21 +309,15 @@ public class MatchWindow extends JPanel implements Runnable{
    }
    
    public void run(){
-      running = true;
       while(running){
          try{
-            if(counting && !hasWinner){
+            if(counting){
                seconds -= 1;
                if(seconds < 0){
                   minutes -= 1;
                   seconds = 59;
                   if(minutes < 0){
                      running = false;
-                     counting = false;
-                     if(!((periodNum == 3 || periodNum == 6) && score1 - score2 != 0)){
-                        periodNum++;
-                        setPeriod();
-                     }
                      minutes = 0;
                      seconds = 0;
                   }
@@ -382,33 +326,11 @@ public class MatchWindow extends JPanel implements Runnable{
                   time.setText(minutes + ":" + "0" + seconds);
                else
                   time.setText(minutes + ":" + seconds);
-               Thread.sleep(100);
+               Thread.sleep(1000);
             }
          }catch(InterruptedException e){
             e.printStackTrace();
          }
-      }
-   }
-   
-   private void setPeriod(){
-      if(periodNum < 8 && periodNum > 0){
-         if(periodNum < 4){
-            period.setText(" Period " + periodNum);
-         }else if(periodNum == 4){
-            period.setText("Overtime");
-         }else if(periodNum == 5){
-            period.setText("Tiebreaker 1");
-         }else if(periodNum == 6){
-            period.setText("Tiebreaker 2");
-         }else if(periodNum == 7){
-            period.setText("Ultimate Tiebreaker");
-         }
-      }else if(periodNum > 0){
-         periodNum--;
-         setPeriod();
-      }else{
-         periodNum++;
-         setPeriod();
       }
    }
    
--- Client/Start.java
+++ Client/Start.java
@@ -3,7 +3,7 @@ import javax.swing.WindowConstants;
 public class Start{
    public Start(){
       JFrame frame = new JFrame("Wrestling");
-      frame.setContentPane(new MatchWindow("Michael", "David"));
+      frame.setContentPane(new MatchWindow());
       frame.pack();
       frame.setResizable(false);
       frame.setLocationRelativeTo(null);
